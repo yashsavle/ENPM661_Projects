bc464e8de341802076999e03514383138cf513d2
yashsavle <55366328+yashsavle@users.noreply.github.com>
2020-02-21 19:18:37 -0500

    Update 8_Puzzle.py
    Optimized the run time using string instead of list.

--- Project1/8_Puzzle.py
+++ Project1/8_Puzzle.py
@@ -1,7 +1,9 @@
-import numpy as np
+import timeit
 
 # Set Goal Configuration of the puzzle
-goal = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+goal = str(123456780)
+
+
 
 #defining lists to be used in the calculations
 init_node= []
@@ -15,9 +17,14 @@ print("Enter Initial Node")
 for i in range(9):
     n= input()
     init_node.append(int(n))
-ct_node=init_node[:]
+start =  timeit.timeit()
+#ct_node=init_node[:]
+#ct_node=np.array(ct_node)
+#nodes.append(init_node)
+s = [str(i) for i in init_node]
+ct_node = str("".join(s))
+ct_node = str(ct_node)
 nodes.append(ct_node)
-
 
 
 #Function to check if the Solution Exists
@@ -33,175 +40,183 @@ def check_sol(l1):
     if(count%2==0):
         print('This case is Solvable')
         print('Please wait, the solution is being formulated')
-        return(1)
+        return 1
     else:
-        return (0)
+        return 0
 
 # Function to find the location of Blank(0) tile
-def BlankTileLocation(l1):
-    x = l1[:]
-    x=np.array(x)
-    x= x.reshape(3,3)
+def BlankTileLocation(x):
 
-    for i in range(0,3):
-        for j in range(0,3):
-            if (x[i, j]==0):
-                return([i,j])
+    for i in range(0,len(x)):
+            #print(x[i])
+            if (x[i]=='0'):
+                #print(type(i))
+                return i
+                break
 
 # Function to Shift Zero to LEFT
 def ActionMoveLeft(x):
-    x = np.array(x)
-    x = x.reshape(3, 3)
-    [a,b] = BlankTileLocation(x)
-    temp =[]
-    if b==0:
-        return ([0,x])
+
+    b = BlankTileLocation(x)
+    #print(b)
+    if b==0 or b==3 or b==6:
+        return [0,x]
     else:
-        temp =x[a,b-1]
-        x[a,b-1]= x[a,b]
-        x[a,b]= temp
-        #print(x)
-        return([1,x])
+        a= list(x)
+        a[b],a[b-1] = a[b-1],a[b]
+        y = ''.join(a)
+       # print(y)
+        return [1,y]
 
 
 # Function to Shift Zero to RIGHT
 def ActionMoveRight(x):
-    x = np.array(x)
-    x = x.reshape(3, 3)
-    [a, b] = BlankTileLocation(x)
-    temp = []
-    if b == 2:
+    #x = l1[:]
+    #x = np.array(x)
+   # x = x.reshape(3, 3)
+    b= BlankTileLocation(x)
+    #print(b)
+    #temp = []
+    if b == 2 or b==5 or b==8:
         return ([0, x])
     else:
-        temp = x[a, b + 1]
-        x[a, b + 1] = x[a, b]
-        x[a, b] = temp
-        return ([1, x])
+        a = list(x)
+        a[b], a[b + 1] = a[b + 1], a[b]
+        y = ''.join(a)
+        #print(y)
+        return ([1, y])
 
 # Function to Shift Zero to UP
 def ActionMoveUp(x):
-    x = np.array(x)
-    x = x.reshape(3, 3)
-    [a, b] = BlankTileLocation(x)
-    temp = []
-    if a == 0:
+    b = BlankTileLocation(x)
+   # print(b)
+    if b == 0 or b== 1 or b==2:
         return ([0, x])
     else:
-        temp = x[a-1, b]
-        x[a-1, b ] = x[a, b]
-        x[a, b] = temp
-        return ([1, x])
+        a = list(x)
+        a[b], a[b - 3] = a[b - 3], a[b]
+        y = ''.join(a)
+        #print(y)
+        return ([1, y])
 
 # Function to Shift Zero to DOWN
 def ActionMoveDown(x):
-    x = np.array(x)
-    x = x.reshape(3, 3)
-    [a, b] = BlankTileLocation(x)
-    temp = []
-    if a == 2:
+    b = BlankTileLocation(x)
+    #print(b)
+    if b == 6 or b==7 or b==8:
         return ([0, x])
     else:
-        temp = x[a+1, b ]
-        x[a+1, b ] = x[a, b]
-        x[a, b] = temp
-        return ([1, x])
+        a = list(x)
+        a[b], a[b + 3] = a[b + 3], a[b]
+        y = ''.join(a)
+        #print('done')
+        return ([1, y])
 
 #Function to check if new node exists in the List of all Nodes
-def exist(n1,n2):
-    for n in range(0,len(n2)):
-        n1= np.reshape(n1,(1,9))
-        if np.array_equal(n1[0],n2[n]):
-            return 1
-
-        return 0
 
 
 # Function to add new node to the node list
-def AddNode(newNode):
-
-    present = exist(newNode,nodes)
+def AddNode(newNode,nodes):
+    for n in nodes:
+        if newNode == n:
+            present =1
+        else:
+            present = 0
     if present==1:
-        return [present]
+        return 1
     else:
-        newNode= newNode.reshape(1,9)
-        newNode= newNode.tolist()
-
-        return [newNode]
+        return (newNode)
 
 # Function to Retrace the node Tree to obtain the solution
 def generate_path(nodes,node_info):
+    #rev = node_info.reverse()
+    #print(rev)
+   # print(nodes)
     add = node_info[-1]
+    #print(add)
     dest =  node_info[0]
-    while add != dest:)
+    #print(dest)
+    while add != dest:
+        #add=index[0]
+        #print(nodes[add[0]])
         solu.append(nodes[add[0]])
         add = node_info[add[1]]
     solu.append(nodes[add[0]])
     return solu
 
 # MAIN
-
 #Check whether Solution Exists
 chk= check_sol(init_node)
 if chk==1:
     v=0
+#for v in range(0,len(nodes)):
+    #print(ct_node)
 
     #Sequence to Generate new nodes by combinations of Left, Right, Up, Down
-    while (np.array_equal(ct_node,goal)== False):
+    while ct_node != goal:
         test = ActionMoveLeft(ct_node)
-        if test[0]==1:
-            [a] = AddNode(test[1])
-            if a!=1:
 
-                nodes.append(a[0])
+        if test[0]==1:
+            a = AddNode(test[1],nodes)
+            if a!=1:
+                nodes.append(a)
                 c = c + 1
                 node_info.append([c,v])
-                goal= np.array(goal)
+               # a=np.array(a)
+               # goal= np.array(goal)
+
 
                 #If Goal node is found break out of loop
-                if np.array_equal(a[0],goal)== True:
+                if a== goal :
+                    #print('exit')
                     break
 
 
         test = ActionMoveRight(ct_node)
-        if test[0] == 1:
-            [a] = AddNode(test[1])
-            if a != 1:
 
-                nodes.append(a[0])
+        if test[0] == 1:
+            a = AddNode(test[1],nodes)
+            if a != 1:
+                nodes.append(a)
                 c = c + 1
                 node_info.append([c, v])
-                goal = np.array(goal)
+                # a=np.array(a)
+                # goal= np.array(goal)
 
                 # If Goal node is found break out of loop
-                if np.array_equal(a[0], goal) == True:
+                if a == goal:
+                    # print('exit')
                     break
 
         test = ActionMoveUp(ct_node)
         if test[0] == 1:
-            [a] = AddNode(test[1])
+            a = AddNode(test[1],nodes)
             if a != 1:
-
-                nodes.append(a[0])
+                nodes.append(a)
                 c = c + 1
                 node_info.append([c, v])
-                goal = np.array(goal)
+                # a=np.array(a)
+                # goal= np.array(goal)
 
                 # If Goal node is found break out of loop
-                if np.array_equal(a[0], goal) == True:
+                if a == goal:
+                    # print('exit')
                     break
 
         test = ActionMoveDown(ct_node)
         if test[0] == 1:
-            [a] = AddNode(test[1])
+            a = AddNode(test[1],nodes)
             if a != 1:
-
-                nodes.append(a[0])
+                nodes.append(a)
                 c = c + 1
                 node_info.append([c, v])
-                goal = np.array(goal)
+                # a=np.array(a)
+                # goal= np.array(goal)
 
                 # If Goal node is found break out of loop
-                if np.array_equal(a[0], goal) == True:
+                #print(a)
+                if a == goal:
+                    print('exit')
                     break
 
         v=v+1
@@ -212,16 +227,27 @@ else:
     print('This case is Unsolvable')
 
 
+#print(nodes)
+#print(node_info)
+
 # Call Function to Generate Solution
 solution = generate_path(nodes,node_info)
 solution.reverse()
 print(solution)
+#ans= check_sol(init_node)
+#print(ans)
+#loc= BlankTileLocation(init_node)
+#print(loc)
 
 # Write Solution in Text File
 nodePath= open('nodePath.txt','w')
-for i in solution:
-    for j in i:
-        nodePath.write('%s '%j)
+count = 0
+for i in range(0,len(solution)):
+    x= solution[i]
+    for count in range(0,3):
+        for j in range(count,len(x),3):
+            var = x[j]
+            nodePath.write('%s '%var)
     nodePath.write("\n")
 nodePath.close()
 
@@ -235,9 +261,16 @@ NodesInfo.close()
 
 # Write All explored nodes in Text file
 n1= open('Nodes.txt','w')
-for i in nodes:
-    for j in i:
-        n1.write('%s '%j)
+for i in range(0,len(nodes)):
+    x= nodes[i]
+    for count in range(0,3):
+        for j in range(count,len(x),3):
+            var = x[j]
+            n1.write('%s '%var)
     n1.write("\n")
 n1.close()
+end =  timeit.timeit()
 
+# Calulate time taken to solve
+delta = end -start
+print(delta)
